06969f432a7d2ea01ecba19e8e4bba133c887093
Luca Massuda <luca.massuda@gmail.com>
2016-03-31 13:17:08 +0200

    Tolto metodo inutile in propertiesFile

--- src/localStorage/PropertiesFile.java
+++ src/localStorage/PropertiesFile.java
@@ -39,31 +39,6 @@ public class PropertiesFile {
 		}
 	}
 	
-	public static void readPropertiesFile(){
-		Properties prop = new Properties();
-		InputStream input = null;
-		try {
-			input = new FileInputStream("config.properties");
-			// load a properties file
-			prop.load(input);
-			// get the property value and print it out
-			System.out.println(prop.getProperty("database"));
-			System.out.println(prop.getProperty("dbuser"));
-			System.out.println(prop.getProperty("dbpassword"));
-
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		} finally {
-			if (input != null) {
-				try {
-					input.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-	
 	public static String getPeoplePath(){
 		String path = null;
 		Properties prop = new Properties();
