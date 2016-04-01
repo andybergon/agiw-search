25bdddc40a647f39b89e578dc762f53552aac89b
Chiara19 <chiara_lombardi@hotmail.it>
2016-04-01 12:15:24 +0200

    600 persone x 8 pagine

--- src/localStorage/PeopleList.java
+++ src/localStorage/PeopleList.java
@@ -14,7 +14,7 @@ import java.util.List;
  *
  */
 public class PeopleList {
-	final static int PEOPLE_NUMBER = 700;
+	final static int PEOPLE_NUMBER = 600;
 	
 	public static List<String> peopleList(String pathFile) throws IOException{
 		List<String> nameList = new ArrayList<String>();
