7c6e9a337e5775fe1059a3be522145c6a25ecfb7
Andrea Bergonzo <andybergon@gmail.com>
2016-04-04 11:28:06 +0200

    automatize PUT into elasticsearch server

--- src/localStorage/HTMLCleaner.java
+++ src/localStorage/HTMLCleaner.java
@@ -1,38 +1,41 @@
 package localStorage;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 
 /**
  * 
- * Classe che pulisce dall HTML una pagina passandogli URL 
- * (ho messo che restituisce il bodi ma si può impostare tutto usando jsoup 
- * aggiungete il jar)
+ * Classe che pulisce dall HTML una pagina passandogli URL (ho messo che
+ * restituisce il bodi ma si puÃ² impostare tutto usando jsoup aggiungete il jar)
  *
  */
 public class HTMLCleaner {
 
-	public static String getBody(String s) throws IOException{
-		/*Document doc;
-			doc = Jsoup.connect(url).get();
-			Elements body = doc.select("body");*/
-		File input = new File(s);
+	public static String getTitle(String filePath) throws IOException {
+		File input = new File(filePath);
 		Document doc = Jsoup.parse(input, "UTF-8", "");
-
-		//System.out.println("body= "+doc.select("body").text());
-		return doc.select("body").text();
+		String title = doc.select("title").text();
+		return title;
+	}
+	
+	public static String getBody(String filePath) throws IOException {
+		File input = new File(filePath);
+		// Jsoup.connect(url).get(); quando si usa?
+		Document doc = Jsoup.parse(input, "UTF-8", "");
+		String body = doc.select("body").text();
+		return body;
 	}
 
-	public static String getTitle(String url) throws IOException{
-		Elements body=Jsoup.parse(url).getElementsByAttribute("title");
-		System.out.println("body= "+body.toString());
-		return body.toString();		
+	public static String[] getTitleAndBody(String filePath) throws IOException {
+		File input = new File(filePath);
+		Document doc = Jsoup.parse(input, "UTF-8", "");
+		String title = doc.select("title").text();
+		String body = doc.select("body").text();
+		String[] data = {title, body};
+		return data;
 	}
+
 }
