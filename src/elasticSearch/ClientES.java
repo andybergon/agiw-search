11b29d35c8e72737871f936c9d9bb26d50363275
Andrea Bergonzo <andybergon@gmail.com>
2016-04-04 16:42:26 +0200

    Revert "provato a fare steamming ma lo ho commentato"
    This reverts commit d47506463136cf3a6d9d29641076ddd8d07b57f8.

--- src/elasticSearch/ClientES.java
+++ src/elasticSearch/ClientES.java
@@ -1,17 +1,15 @@
 package elasticSearch;
 
 import static org.elasticsearch.node.NodeBuilder.*;
-import static org.elasticsearch.common.xcontent.XContentFactory.*;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLDecoder;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import org.elasticsearch.action.admin.indices.create.CreateIndexRequestBuilder;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequestBuilder;
 import org.elasticsearch.action.index.IndexResponse;
@@ -19,19 +17,14 @@ import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.node.Node;
 import org.elasticsearch.node.NodeBuilder;
 import org.elasticsearch.search.SearchHit;
 
 import localStorage.HTMLCleaner;
-import localStorage.PeopleList;
 import localStorage.PropertiesFile;
-import localStorage.TextFileCreator;
-import net.billylieurance.azuresearch.AzureSearchResultSet;
-import net.billylieurance.azuresearch.AzureSearchWebQuery;
-import net.billylieurance.azuresearch.AzureSearchWebResult;
+
 
 public class ClientES {
 	public static void main(String[] args) throws IOException {
@@ -39,59 +32,17 @@ public class ClientES {
 	}
 
 	public static void clientGetter() throws IOException {
-/*
-		Settings settings = Settings.settingsBuilder().loadFromSource(jsonBuilder()
-                .startObject()
-                .startObject("analysis")
-                    .startObject("filter")
-                        .startObject("test_filter_stopwords_it")
-                            .field("type", "stop")
-                            .field("stopwords_path", "stopwords/stop_it")
-                        .endObject()
-                        .startObject("test_filter_snowball_it")
-                            .field("type", "snowball")
-                            .field("language", "Italian")
-                        .endObject()
-                        .startObject("test_filter_worddelimiter_it")
-                            .field("type", "word_delimiter")
-                            .field("protected_words_path", "worddelimiters/protectedwords_it")
-                            .field("type_table_path", "typetable")
-                        .endObject()
-                        .startObject("test_filter_synonyms_it")
-                            .field("type", "synonym")
-                            .field("synonyms_path", "synonyms/synonyms_it")
-                            .field("ignore_case", true)
-                            .field("expand", true)
-                        .endObject()
-                        .startObject("test_filter_ngram")
-                            .field("type", "edgeNGram")
-                            .field("min_gram", 2)
-                            .field("max_gram", 30)
-                        .endObject()
-                   .endObject()
-                   .startObject("analyzer")
-                        .startObject("test_analyzer")
-                            .field("type", "custom")
-                            .field("tokenizer", "whitespace")
-                            .field("filter", new String[]{"lowercase",
-                                                                             "test_filter_worddelimiter_it",
-                                                                             "test_filter_stopwords_it",
-                                                                             "test_filter_synonyms_it",
-                                                                             "test_filter_snowball_it"})
-                            .field("char_filter", "html_strip")
-                        .endObject()
-                   .endObject()
-                .endObject()
-            .endObject().string()).build();
-*/
+
 		// on startup
-		Node node = nodeBuilder().clusterName("elasticsearch").settings(Settings.settingsBuilder()
-				.put("http.enabled", false).put("path.home", "/home/andybergon/elasticsearch-2.2.1/data")).client(true)
+		Node node = nodeBuilder()
+				.clusterName("elasticsearch")
+				.settings(Settings.settingsBuilder()
+				.put("http.enabled", false)
+				.put("path.home", "/home/andybergon/elasticsearch-2.2.1/data"))
+				.client(true)
 				.node();
 
 		Client client = node.client();
-		/*CreateIndexRequestBuilder createIndexRequestBuilder = client.admin().indices().prepareCreate("people");
-		createIndexRequestBuilder.setSettings(settings);*/
 		directoryIterator(client);
 		/*
 		client.prepareIndex("people", "person", "1")
@@ -123,32 +74,20 @@ public class ClientES {
 		File dir = new File(PropertiesFile.getStoragePath());
 		File[] directoryListing = dir.listFiles();
 		if (directoryListing != null) {
-			for (File child : directoryListing) {
+			for (File file : directoryListing) {
 				// Do something with child
-				String fileName = child.getName();
+				String fileName = file.getName();
 				fileName = fileName.replace(".txt", "");
-				String lastname = fileName.split("_")[0];
-				String name = fileName.split("_")[1];
-				//String url = fileName.split("_")[2];
-				int first = fileName.indexOf("_");
-				int second = fileName.indexOf("_", first+1);
-				String url = fileName.substring(second+1);
-				String urlOriginal = URLDecoder.decode(url, "UTF-8");
-				System.out.println(lastname+" "+name+" "+urlOriginal);
-				
-				/*String json = "{" +"\"name\":\""+name+"\"," +
-				        "\"lastname\":\""+lastname+"\"," +
-				        "\"body\":\""+HTMLCleaner.getBody(child.getAbsolutePath())+"\"" +
-				        "\"url\":\""+urlOriginal+"\"," +
-				    "}";
-
+				String[] fileInfo = fileName.split("_",3);
+				String url = fileInfo[2];
+				String urlDecoded = URLDecoder.decode(url, "UTF-8");
+				String filePath = file.getAbsolutePath();
+				String[] data = HTMLCleaner.getTitleAndBody(filePath);
+				String title = data[0];
+				String body = data[1];
 				client.prepareIndex("people", "person")
-				        .setSource(json)
-				        .execute().actionGet().isCreated();*/
-				client.prepareIndex("people", "person")
-				.setSource(putJsonDocument(name+" "+lastname, HTMLCleaner.getBody(child.getAbsolutePath()),	
-						new URL(urlOriginal)))
-				.execute().actionGet().isCreated();
+						.setSource(putJsonDocument(title, body,	new URL(urlDecoded)))
+						.execute().actionGet().isCreated();
 			}
 		} else {
 			System.out.println("Storage Path not setted properly, it should be a directory!");
