b4c81322bb66a170aeca6cfd85c6d149ca8458bf
Andrea Bergonzo <andybergon@gmail.com>
2016-04-09 16:23:14 +0200

    add pages logic and google like titles

--- website/js/search.js
+++ website/js/search.js
@@ -1,111 +1,82 @@
+var baseUrl = "http://localhost:9200/people/_search?q=";
+var query, firstResult, showPerPage, maxPages, url, size, fromVar, currentPage;
+var totalHits, pagesNumber, persons, pagesToDisplay, maxTitleLength;
+
+showPerPage = 10;
+maxPages = 20;
+size = "&size=" + showPerPage;
+
+maxTitleLength = 60;
+
 $(document).ready(function() {
 	$("#search_button").click(function(){
-		var url = "http://localhost:9200/people/_search?q=";
-		var query = "";
-		var showPerPage = 10;
-		var firstResult = 0;
-		var size = "&size=" + showPerPage;
-		var fromVar = "&from=" + firstResult
-
 		query = $("#search_bar").val();
-		window.queryGlobal = query; // TODO:workaround
-		url = url + query + size + fromVar;
-		console.log(url);
-
-		$('#results').empty(); // clear previous results
-
-		$.getJSON(url, function(result){
-			console.log(result);
-			var totalHits = result.hits.total;
-			var pagesNumber = Math.ceil(totalHits/showPerPage);
-			var persons = result.hits.hits;
-			// oppure: $.each(result, function(i, field){$("div").append(field + " "); });
-			for (var i = 0; i < showPerPage; i++) {
-				// TODO: eliminare log in production
-				console.log(persons[i]);
-				title = persons[i]._source.title;
-				url = persons[i]._source.url;
-				var regex = /^(https?|ftp):\/\//;
-				url = url.replace(regex,'');
-				content = persons[i]._source.content;
-				queryString = " "+ query + " ";
-				indexQuery = content.search(queryString);
-				if (indexQuery!=-1){
-					desc1 = content.substring(indexQuery-50, indexQuery);
-					descQ = content.substring(indexQuery, indexQuery+query.length+2);
-					descQ = descQ.bold();
-					desc2 = content.substring(indexQuery+query.length, indexQuery+query.length+50);
-					desc = desc1 + descQ + desc2;
-				} else {
-					desc = content.substring(0, 100);
-				}
-				// oppure: $('<a\>', {prop: value}).appendTo('#results');
-				var a = '<a class="result-title" href="' + url + '">' + title + '</a>';
-				var p = '<p class="result-url">' + url + '</p>';
-				var p2 = '<p class="result-desc">' +"..."+ desc + "..." + '</p>';
-				$('#results').append('<div class="result">' + a + p + p2 + '</div>');
-			}
-
-			var checkBug = 20; //TODO
-			for (var i = 0; i < checkBug; i++) {
-				var url = "changePage(" + (i+1) + ")";
-				var a = '<a class="result-page" href="#" onclick="' + url + '">' + "  "+(i+1) + '</a>';
-				$('.result-pages').append(a);
-				console.log("adad");
-			}
-
-		});
+		currentPage = 1;
+		getES();
 	});
 });
 
-function changePage(pageNum) {
-	//$(".result-page").click(function(){
-		var url = "http://localhost:9200/people/_search?q=";
-		var query = window.queryGlobal; //TODO:workaround
-		var showPerPage = 10;
-		var firstResult = (pageNum-1)*showPerPage;
-		var size = "&size=" + showPerPage;
-		var fromVar = "&from=" + firstResult
-
-		query = $("#search_bar").val();
-		url = url + query + size + fromVar;
-		console.log(url);
-
+function getES() {
+	firstResult = (currentPage-1) * showPerPage;
+	fromVar = "&from=" + firstResult;
+	url = baseUrl + query + size + fromVar;
+	console.log(url);
+	$.getJSON(url, function(result){
 		$('#results').empty(); // clear previous results
-		console.log("clearing old results");
-
-		$.getJSON(url, function(result){
-			console.log(result);
-			var totalHits = result.hits.total;
-			var pagesNumber = Math.ceil(totalHits/showPerPage);
-			var persons = result.hits.hits;
-			// oppure: $.each(result, function(i, field){$("div").append(field + " "); });
-			for (var i = 0; i < showPerPage; i++) {
-				// TODO: eliminare log in production
-				console.log(persons[i]);
-				title = persons[i]._source.title;
-				url = persons[i]._source.url;
-				var regex = /^(https?|ftp):\/\//;
-				url = url.replace(regex,'');
-				content = persons[i]._source.content;
-				queryString = " "+ query + " ";
-				indexQuery = content.search(queryString);
-				if (indexQuery!=-1){
-					desc1 = content.substring(indexQuery-50, indexQuery);
-					descQ = content.substring(indexQuery, indexQuery+query.length+2);
-					descQ = descQ.bold();
-					desc2 = content.substring(indexQuery+query.length, indexQuery+query.length+50);
-					desc = desc1 + descQ + desc2;
-				} else {
-					desc = content.substring(0, 100);
-				}
-				// oppure: $('<a\>', {prop: value}).appendTo('#results');
-				var a = '<a class="result-title" href="' + url + '">' + title + '</a>';
-				var p = '<p class="result-url">' + url + '</p>';
-				var p2 = '<p class="result-desc">' +"..."+ desc + "..." + '</p>';
-				$('#results').append('<div class="result">' + a + p + p2 + '</div>');
+		$('.result-pages').empty();
+		console.log(result);
+		// con db statico le prossime 2 potrei non ricalcolarle
+		totalHits = result.hits.total;
+		pagesNumber = Math.ceil(totalHits/showPerPage);
+		persons = result.hits.hits;
+		// oppure: $.each(result, function(i, field){$("div").append(field + " "); });
+		for (var i = 0; i < persons.length; i++) {
+			console.log(persons[i]);
+			title = persons[i]._source.title;
+			// cut too long titles
+			if (title.length > maxTitleLength) {
+				var start = title.substring(0, maxTitleLength);
+				var lastSpaceToVisIndex = title.substring(maxTitleLength).indexOf(" ");
+				var end = title.substring(maxTitleLength, maxTitleLength + lastSpaceToVisIndex + 1);
+				title = start + end + " ...";
 			}
+			url = persons[i]._source.url;
+			var regex = /^(https?|ftp):\/\//;
+			var urlClean = url.replace(regex,'');
+			content = persons[i]._source.content;
+			// var queryString = new RegExp("^" + query + " | " + query + " ");
+			queryString = " " + query + " "; // TODO: se è all'inizio del testo non funziona
+			indexQuery = content.search(queryString);
+			if (indexQuery!=-1){
+				desc1 = content.substring(indexQuery-50, indexQuery);
+				descQ = content.substring(indexQuery, indexQuery+query.length+2);
+				descQ = descQ.bold();
+				desc2 = content.substring(indexQuery+query.length, indexQuery+query.length+50);
+				desc = desc1 + descQ + desc2;
+			} else {
+				desc = content.substring(0, 100);
+			}
+			// oppure: $('<a\>', {prop: value}).appendTo('#results');
+			var a = '<a class="result-title" href="' + url + '">' + title + '</a>';
+			var p = '<p class="result-url">' + urlClean + '</p>';
+			var p2 = '<p class="result-desc">' +"..."+ desc + "..." + '</p>';
+			$('#results').append('<div class="result">' + a + p + p2 + '</div>');
+		}
 
-		});
-	//});
+		// can be done simply modifing the css instead of recalculating it every time
+		pagesToDisplay = pagesNumber < maxPages ? pagesNumber : maxPages;
+		for (var i = 1; i <= pagesToDisplay; i++) {
+			var url, a, inactive;
+			url = "changePage(" + i + ")";
+			inactive = (i == currentPage) ? " inactive-link" : "";
+			a = '<a class="result-page' + inactive + '" href="#" onclick="' + url + '">' + i + '</a>';
+			$('.result-pages').append(a);
+		}
+		console.log("pages appended");
+	});
+}
+
+function changePage(pageNum) {
+		currentPage = pageNum;
+		getES();
 }
